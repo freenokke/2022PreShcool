26658e90095ad593535a4bd511d9e470045b8b1d
freenokke <kosmatkov94@gmail.com>
2022-02-03 14:45:26 +0300

    fix: improve 'updateTime' function

--- audio-player/js/index.js
+++ audio-player/js/index.js
@@ -1,5 +1,5 @@
 //объявление переменных
-const audioTag = document.querySelector('audio');
+const audio = new Audio('./audio/lusia.mp3');
 const progressBar = document.querySelector('input');
 const prevBtn = document.querySelector('.toolbar__previous-btn')
 const playBtn = document.querySelector('.toolbar__play-btn')
@@ -10,8 +10,8 @@ const currentTimeMinutes = document.querySelector('.current-time__minutes')
 const currentTimeSeconds = document.querySelector('.current-time__seconds')
 
 function togglePlayAudio() {
-    let method = audioTag.paused ? 'play' : 'pause';
-    audioTag[method]();
+    let method = audio.paused ? 'play' : 'pause';
+    audio[method]();
 }
 
 function updateProgress(e) {
@@ -20,42 +20,37 @@ function updateProgress(e) {
 }
 
 function updateTime() {
-    const intMinCount = currentTimeMinutes.textContent.length;
-    const intSecCount = currentTimeSeconds.textContent.length;
-
-    if (intMinCount == 2) {
-        currentTimeMinutes.textContent = Math.trunc(audioTag.currentTime / 60)
-    } else {
-        currentTimeSeconds.textContent = currentTimeSeconds.textContent.padStart(Math.trunc(audioTag.currentTime / 60), '0')
-    }
-
-    if(intSecCount == 2) {
-        currentTimeSeconds.textContent = Math.trunc(audioTag.currentTime % 60)
-    } else {
-        currentTimeSeconds.textContent = currentTimeSeconds.textContent.padStart(Math.trunc(audioTag.currentTime % 60), '0')
-    }
+    currentTimeMinutes.textContent = Math.trunc(audio.currentTime / 60).toString().padStart('2', 0)
+    currentTimeSeconds.textContent = Math.trunc(audio.currentTime % 60).toString().padStart('2', 0)
+    
 }
 
 function updateProgressOnClick() {
-    audioTag.currentTime = this.value;
+    audio.currentTime = this.value;
 }
 
 //Обработчики
     //Обновление прогресса трека от текущего времени
-audioTag.addEventListener('timeupdate', updateProgress);
+audio.addEventListener('timeupdate', updateProgress);
+
     //Обновление текущего времени трека
-audioTag.addEventListener('timeupdate', updateTime);
+audio.addEventListener('timeupdate', updateTime);
+
     //Работа кнопок
 playBtn.addEventListener('click', togglePlayAudio);
+
     //Изменение прогресса трека по клику
 progressBar.addEventListener('input', updateProgressOnClick);
+
     //Подгрузка продолжительности трека в инпут при загрузке страницы
 document.addEventListener('DOMContentLoaded', () => {
-    progressBar.setAttribute('max', audioTag.duration);
-    totalTimeMinutes.textContent = '0' + Math.trunc(audioTag.duration / 60)
-    totalTimeSeconds.textContent = Math.trunc(audioTag.duration % 60)
+    progressBar.setAttribute('max', audio.duration);
+    totalTimeMinutes.textContent = '0' + Math.trunc(audio.duration / 60)
+    totalTimeSeconds.textContent = Math.trunc(audio.duration % 60)
+
 })
+
     //Подгрузка продолжительности трека в инпут при изменении трека
-audioTag.addEventListener('durationchange', () => {
-    progressBar.setAttribute('max', audioTag.duration);
+audio.addEventListener('durationchange', () => {
+    progressBar.setAttribute('max', audio.duration);
 })
