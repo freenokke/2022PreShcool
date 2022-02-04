7f4211acc21e811c3df59758945fd36a2ea9d2b6
freenokke <kosmatkov94@gmail.com>
2022-02-04 13:08:42 +0300

    feat: update playList.js

--- audio-player/js/playList.js
+++ audio-player/js/playList.js
@@ -3,21 +3,25 @@ const playList = [
     author: 'Люся Чеботина',
     title: 'Монако',
     src: './audio/lusia.mp3',
+    poster: './img/lyusya-chebotina.jpg',
 },
 {      
     author: 'Galibri & Mavik',
     title: 'Федерико Феллини',
     src: './audio/felini.mp3',
+    poster: './img/galibri.jpg',
 },  
 {      
     author: 'MiyaGi',
     title: 'Самурай',
     src: './audio/samurai.mp3',
+    poster: './img/miyagi.jpg',
 },
 {      
     author: 'Niman feat. Truwer & Райда & Скриптонит',
     title: 'Талия',
     src: './audio/taliya.mp3',
+    poster: './img/skriptonit.jpg',
 },
 ]
 export default playList;
