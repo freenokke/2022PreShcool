608d4b60b3c969e7a5a96f53f12ffaf9e59e21ae
freenokke <kosmatkov94@gmail.com>
2022-02-05 15:34:15 +0300

    feat: add responsible volume range

--- audio-player/js/index.js
+++ audio-player/js/index.js
@@ -2,10 +2,12 @@ import playList from "./playList.js";
 
 //объявление переменных
 const audio = new Audio();
-const progressBar = document.querySelector('input');
 const prevBtn = document.querySelector('.toolbar__previous-btn')
+const progressBar = document.querySelector('.inp');
+const volumeBar = document.querySelector('.inp-volume');
 const playBtn = document.querySelector('.toolbar__play-btn')
 const nextBtn = document.querySelector('.toolbar__next-btn')
+const volumeBtn = document.querySelector('.player__volume-btn')
 const totalTimeMinutes = document.querySelector('.total-time__minutes')
 const totalTimeSeconds = document.querySelector('.total-time__seconds')
 const currentTimeMinutes = document.querySelector('.current-time__minutes')
@@ -25,10 +27,39 @@ function changePlayIcon() {
     if (audio.paused) {playBtn.style.backgroundImage = `url(./svg/play.svg)`}
 }
 
+function changeVolumeIcon(e) {
+    if (volumeBar.value == 0) {
+        volumeBtn.style.backgroundImage = `url(./svg/volume.svg)`
+        const value = 0.5
+        volumeBar.value = value
+        audio.volume = value
+        volumeBar.style.background = `linear-gradient(to right, #C6A780 0%, #C6A780 50%, #92a8af 50%, #92a8af 100%)`
+    } else {
+        volumeBtn.style.backgroundImage = `url(./svg/mute.svg)`
+        const value = 0
+        volumeBar.value = value
+        audio.volume = value
+        volumeBar.style.background = `linear-gradient(to right, #C6A780 0%, #C6A780 ${value}%, #92a8af ${value}%, #92a8af 100%)`
+    }
+    
+    
+
+}
+
 function updateProgress(e) {
     progressBar.value = e.srcElement.currentTime //для ползунка
     const value = e.srcElement.currentTime / e.srcElement.duration * 100 // для бэкраунда
     progressBar.style.background = `linear-gradient(to right, #C6A780 0%, #C6A780 ${value}%, #92a8af ${value}%, #92a8af 100%)` // для бэкраунда
+}
+
+function updateVolumeProgress() {
+    audio.volume = this.value
+    const value = this.value * 100
+    volumeBar.style.background = `linear-gradient(to right, #C6A780 0%, #C6A780 ${value}%, #92a8af ${value}%, #92a8af 100%)`
+    // смена иконки если звука нет/есть соответственно
+    if (volumeBar.value == 0) {volumeBtn.style.backgroundImage = `url(./svg/mute.svg)`} else {
+        volumeBtn.style.backgroundImage = `url(./svg/volume.svg)`
+    }
 }
 
 function updateProgressOnClick() {
@@ -83,23 +114,28 @@ function preload() {
     trackIMG.style.backgroundImage = `url(${playList[0].poster})`;
 }
 
+
 //Обработчики
     //Обновление прогресса трека от текущего времени
 audio.addEventListener('timeupdate', updateProgress);
+
+    //Измкенение громкости
+volumeBar.addEventListener('input', updateVolumeProgress)
 
     //Работа кнопок
 playBtn.addEventListener('click', togglePlayAudio);
 nextBtn.addEventListener('click', nextAudio);
 prevBtn.addEventListener('click', previousAudio);
+volumeBtn.addEventListener('click', changeVolumeIcon);
 
     //Изменение прогресса трека по клику
 progressBar.addEventListener('input', updateProgressOnClick);
     // Переключения трека, когда текущий закончился
 audio.addEventListener('ended', nextAudio)
 
-    //подгрузка значений продолжительности трека и запуск обработчика событий текущего времени
+    //подгрузка значений продолжительности трека, звука и запуск обработчика событий текущего времени
 audio.addEventListener('loadeddata', (e) => {
-    progressBar.setAttribute('max', audio.duration);
+    progressBar.setAttribute('max', audio.duration); // продолжительность
     function updateTime(ev) {
         currentTimeMinutes.textContent = Math.trunc(audio.currentTime / 60).toString().padStart('2', 0)
         currentTimeSeconds.textContent = Math.trunc(audio.currentTime % 60).toString().padStart('2', 0)
@@ -110,6 +146,8 @@ audio.addEventListener('loadeddata', (e) => {
     //Обновление текущего времени трека
     audio.addEventListener('timeupdate', updateTime);
 })
+
+
     //Подгрузка первого трека при загрузке / обновлении страницы
 document.addEventListener('DOMContentLoaded', preload)
 //     //Подгрузка продолжительности трека в инпут при изменении трека
