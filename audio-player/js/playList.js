4201644f574158800591a1e3e9402249f81ba1cc
freenokke <kosmatkov94@gmail.com>
2022-02-05 16:31:57 +0300

    refactor: update playlist

--- audio-player/js/playList.js
+++ audio-player/js/playList.js
@@ -1,27 +1,45 @@
 const playList = [
 {      
-    author: 'Люся Чеботина',
-    title: 'Монако',
-    src: './audio/lusia.mp3',
-    poster: './img/lyusya-chebotina.jpg',
+    author: 'NEFEX',
+    title: 'Cold',
+    src: './audio/Neffex_-_Cold_(musmore.com).mp3',
+    poster: './img/nefex.jpg',
 },
 {      
-    author: 'Galibri & Mavik',
-    title: 'Федерико Феллини',
-    src: './audio/felini.mp3',
-    poster: './img/galibri.jpg',
+    author: 'Imagine Dragons',
+    title: 'Enemy',
+    src: './audio/Imagine_Dragons_JID_League_of_Legends_-_Enemy_(musmore.com).mp3',
+    poster: './img/enemy.jpg',
 },  
+{      
+    author: 'Jaxson Gamble',
+    title: 'Dynamite',
+    src: './audio/Jaxson Gamble - Dynamite (radio.lol).mp3',
+    poster: './img/dynamite.jpg',
+},
+{      
+    author: 'NEFEX',
+    title: 'NO TURNING BACK',
+    src: './audio/NEFFEX_-_NO_TURNING_BACK_(musmore.com).mp3',
+    poster: './img/nefextake.jpg',
+},
+{      
+    author: 'Скриптонит и Райда',
+    title: 'Baby mama',
+    src: './audio/Skriptonit_Rajjda_-_Baby_mama_(musmore.com).mp3',
+    poster: './img/babymama.jpg',
+},
+{      
+    author: 'Jaxson Gamble',
+    title: 'Fresh',
+    src: './audio/JAXSON_GAMBLE_-_Fresh_(musmore.com).mp3',
+    poster: './img/fresh.jpg',
+},
 {      
     author: 'MiyaGi',
     title: 'Самурай',
     src: './audio/samurai.mp3',
     poster: './img/miyagi.jpg',
-},
-{      
-    author: 'Niman feat. Truwer & Райда & Скриптонит',
-    title: 'Талия',
-    src: './audio/taliya.mp3',
-    poster: './img/skriptonit.jpg',
 },
 ]
 export default playList;
