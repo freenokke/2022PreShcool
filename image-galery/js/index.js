c8cf30dac0c388245658975644bad4c638a6d97c
freenokke <kosmatkov94@gmail.com>
2022-02-13 20:43:21 +0300

    feat: add input & possibility of changing images via query

--- image-galery/js/index.js
+++ image-galery/js/index.js
@@ -1,13 +1,11 @@
-const container = document.querySelector('.container')
+const container = document.querySelector('.container-image')
+const input = document.querySelector('input')
 
 function createElem(src) {
     const img = document.createElement('img')
     img.src = src
     container.append(img)
 }
-
-
-
 
 async function getData() {
     const res = await fetch('https://api.unsplash.com/photos/random?client_id=bIvEWFhyHtf9BBEeCLa4CaqZ1-hXCUbNXQDcvwpZfcI&count=12');
@@ -17,6 +15,21 @@ async function getData() {
         createElem(element.urls.regular)
     });
 }
-
-
 getData();
+
+async function search() {
+    const res = await fetch(`https://api.unsplash.com/search/photos?client_id=bIvEWFhyHtf9BBEeCLa4CaqZ1-hXCUbNXQDcvwpZfcI&page=1&per_page=12&query=${input.value}`);
+    const data = await res.json();
+
+    const images = document.querySelectorAll('img')
+    for (let i = 0; i < images.length; i++) {
+        images[i].src = data.results[i].urls.regular
+    } 
+}
+
+input.addEventListener('keyup', function(event) {
+    if(event.key == 'Enter'){
+        search();
+        input.value = '';
+    }
+})
